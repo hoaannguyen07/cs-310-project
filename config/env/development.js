3891026648177f40cae5d1ff750476a05a78974a
Hoa Nguyen <hoaannguyen07@gmail.com>
2022-04-06 16:32:26 -0500

    put wrong db address

--- config/env/development.js
+++ config/env/development.js
@@ -2,8 +2,8 @@ module.exports = {
     db: {
         user: "postgres",
         password: "postgres",
-        database: "db-310-dev",
-        host: "db-310-dev.crd5xwkqdc4a.us-east-1.rds.amazonaws.com",
+        database: "cs_310_db",
+        host: "cs-310-db.c9ncqlvp1kw9.us-east-2.rds.amazonaws.com",
         port: 5432,
         max: 50,
         idleTimeoutMillis: 30000,
