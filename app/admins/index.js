a94dc73210aa3e22a56a72e29fec819d6c58c830
Zach <zsum19@tamu.edu>
2022-04-25 16:15:53 -0500

    approving posts

--- app/admins/index.js
+++ app/admins/index.js
@@ -1,5 +1,64 @@
+const db = require("../../db");
+
 module.exports = {
     renderAdminLanding: (req, res) => {
         res.render("admin/admin_landing");
+    },
+
+    renderUnapprovedPostsPage: (req, res) => {
+        db.query(
+            "SELECT id, title, body, created_at FROM unapproved_posts ORDER BY created_at DESC",
+            [],
+            (err, result) => {
+                if (err) {
+                    req.flash("error", "Unable to query unapproved posts");
+                    return res.render("admin/admin_post_approval");
+                }
+                console.log(result.rows)
+                return res.render("admin/admin_post_approval", {
+                    unapproved_posts: result.rows,
+                });
+            }
+        );
+    },
+
+    approveUnapprovedPost: (req, res) => {
+        let { id } = req.params;
+        try {
+            id = parseInt(id);
+        } catch (error) {
+            req.flash("error", "Unable to query post");
+            return res.redirect("/admin_post_approval");
+        }
+        db.query(
+            "INSERT INTO posts SELECT * FROM unapproved_posts WHERE id=$1;",
+            [id],
+            (err, result) => {
+                if (err || result.rowCount !== 1) {
+                    req.flash(
+                        "error",
+                        `Error Approving Post. Error Code: ${err.code}`
+                    );
+                }
+
+                req.flash("success", `Successfully approved post ${id}!`);
+            }
+        );
+
+        db.query(
+            "DELETE FROM unapproved_posts WHERE id=$1",
+            [id],
+            (err, result) => {
+                if (err) {
+                    req.flash("error", `Unable to delete post ${id}`);
+                } else {
+                    req.flash(
+                        "success",
+                        `Successfully deleted unapproved post from table`
+                    );
+                }
+                return res.redirect("/admin_post_approval");
+            }
+        );
     },
 };
